adeb9b600217daa7607804627c236d87bf07c2ae
Kenny Marechal <kennymarechal@gmail.com>
2020-05-29 10:57:20 -0400

    Modification de dashbord
    Modification de dashbord

--- administration/index.php
+++ administration/index.php
@@ -17,7 +17,30 @@
             <?php include "visites.php"?>
         </a>
     </article> 
-    
+    <p>
+        La suprême ironie de la vie c'est que nul n'en sort vivant. 
+        <br>Robert A. Heinlein 
+        <br>Artiste, écrivain (1907 - 1988)
+        <br><br>
+        Lorsque l'on meurt ou que l'on souffre d'une horrible perte nous traversons tous 5 étapes de la peine. <br><br>
+
+(étape 1 : le déni)
+On passe par le déni car la perte n'est pas envisageable, on ne peut imaginer que c'est réel. <br><br>
+
+(étape 2 : la colère)
+On s'énerve contre tout le monde, contre les survivants, contre nous-même. <br><br>
+
+(étape 3 : la négociation)
+Puis on trouve un arrangement, on supplie, on implore, on offre tout ce qu'on a, on offre nos propres âmes en échange d'un jour de plus. <br><br>
+
+(étape 4 : la dépression)
+Quand les négociations échouent la colère est difficile à contenir, on tombe dans la dépression, le désespoir, jusqu'à ce qu'on accepte finalement que l'on a tout tenté. <br><br>
+
+(étape 5 : l'acceptation)
+On abandonne. On abandonne et on accepte. <br><br>
+
+Le chagrin peut être une chose que l'on a en commun mais il est différent pour tout le monde.
+    </p>
     <article id="box"> 
     </article>
 </section>
